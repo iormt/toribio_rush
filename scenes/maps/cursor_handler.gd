22ccc46f1e8c3d40372caa5c6cd325660c48c485
fedemello <fedemg15@gmail.com>
2025-01-26 02:39:22 -0300

    cursor handler

---
+++ scenes/maps/cursor_handler.gd
@@ -0,0 +1,20 @@
+extends Node3D
+
+
+# Load the custom images for the mouse cursor.
+var arrow = load("res://arrow.png")
+var beam = load("res://beam.png")
+
+
+func _ready():
+	# Changes only the arrow shape of the cursor.
+	# This is similar to changing it in the project settings.
+	Input.set_custom_mouse_cursor(arrow)
+
+	# Changes a specific shape of the cursor (here, the I-beam shape).
+	#Input.set_custom_mouse_cursor(beam, Input.CURSOR_IBEAM)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
